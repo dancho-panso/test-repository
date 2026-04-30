69a5878e8e7ede3c1f4783e0858b2c309ddbc983
Данил Михайловский <danilmihailovskii@mail.ru>
2026-04-30 17:23:55 +0300

    init project

---
+++ src/masks.py
@@ -0,0 +1,19 @@
+def get_mask_card_number(card_number: str) -> str:
+    '''Функция маскировки карты'''
+    # Проверяем длину номера карты
+    if len(card_number) != 16:
+        raise ValueError("Номер карты должен содержать 16 цифр.")
+    # Форматируем номер карты
+    masked_card = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
+    return masked_card
+
+
+def get_mask_account(account_number: str) -> str:
+    '''Функция маскировки счета'''
+    # Проверяем длину номера счета
+    if len(account_number) < 4:
+        raise ValueError("Номер счета должен содержать как минимум 4 цифры.")
+    # Форматируем номер счета
+    masked_account = f"**{account_number[-4:]}"
+    return masked_account
+
